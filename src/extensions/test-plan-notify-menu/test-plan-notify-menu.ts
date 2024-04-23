6b6a7ebe015e82fd49838d86f23b006f0fae4884
shaiju <rajan.shaiju@outlook.com>
2024-04-23 17:10:11 +0530

    menu loaded but popup erroring out

--- src/extensions/test-plan-notify-menu/test-plan-notify-menu.ts
+++ src/extensions/test-plan-notify-menu/test-plan-notify-menu.ts
@@ -1,14 +1,16 @@
 import "es6-promise/auto";
 import * as SDK from "azure-devops-extension-sdk";
 import { CommonServiceIds, getClient, IHostPageLayoutService } from "azure-devops-extension-api";
-import { BuildDefinition, BuildRestClient } from "azure-devops-extension-api/Build";
+/*import { BuildDefinition, BuildRestClient } from "azure-devops-extension-api/Build";*/
 
 SDK.register("sample-build-menu", () => {
     return {
-        execute: async (context: BuildDefinition) => {
-            const result = await getClient(BuildRestClient).getDefinition(context.project.id, context.id, undefined, undefined, undefined, true);
+        execute: async (/*context: BuildDefinition*/) => {
+            /*const result = await getClient(BuildRestClient).getDefinition(context.project.id, context.id, undefined, undefined, undefined, true);*/
+            const result={name: "Shaiju"}
             const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
-            dialogSvc.openMessageDialog(`Fetched build definition ${result.name}. Latest build: ${JSON.stringify(result.latestBuild)}`, { showCancel: false });
+            /*dialogSvc.openMessageDialog(`Fetched build definition ${result.name}. Latest build: ${JSON.stringify(result.latestBuild)}`, { showCancel: false });*/
+            dialogSvc.openMessageDialog(`Name: ${result.name}`, { showCancel: false });
         }
     }
 });
