2dc6bc883f9c1848afefd8ac27440894f3ef907e
shaiju <rajan.shaiju@outlook.com>
2024-04-24 07:24:41 +0530

    menu without errors

--- src/extensions/test-plan-notify-menu/test-plan-notify-menu.ts
+++ src/extensions/test-plan-notify-menu/test-plan-notify-menu.ts
@@ -1,18 +1,34 @@
 import "es6-promise/auto";
 import * as SDK from "azure-devops-extension-sdk";
 import { CommonServiceIds, getClient, IHostPageLayoutService } from "azure-devops-extension-api";
-/*import { BuildDefinition, BuildRestClient } from "azure-devops-extension-api/Build";*/
+import { WorkItemTrackingRestClient, IWorkItemFormService} from "azure-devops-extension-api/WorkItemTracking";
+import { TestPoint, TestRestClient } from "azure-devops-extension-api/Test";
 
-SDK.register("sample-build-menu", () => {
+
+SDK.register("notify-next-test-case-assignee", () => {
     return {
-        execute: async (/*context: BuildDefinition*/) => {
-            /*const result = await getClient(BuildRestClient).getDefinition(context.project.id, context.id, undefined, undefined, undefined, true);*/
+        execute: async (context: TestPoint[]) => {
+
+            const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
+            dialogSvc.openMessageDialog(`${context[0].assignedTo} notified!!`, { showCancel: false });
+        }
+    }
+});
+
+/*SDK.register("notify-next-test-case-assignee", () => {
+    return {
+        execute: async (context: any[]) => {
+            debugger;
+            console.log(context[0].System.Title);
+            const result = await getClient(BuildRestClient).getDefinition(context.project.id, context.id, undefined, undefined, undefined, true);
             const result={name: "Shaiju"}
             const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
-            /*dialogSvc.openMessageDialog(`Fetched build definition ${result.name}. Latest build: ${JSON.stringify(result.latestBuild)}`, { showCancel: false });*/
+            dialogSvc.openMessageDialog(`Fetched build definition ${result.name}. Latest build: ${JSON.stringify(result.latestBuild)}`, { showCancel: false });
+            dialogSvc.openMessageDialog(`Name: ${result.name}, testSuiteId:${context.suite.name}`, { showCancel: false });
             dialogSvc.openMessageDialog(`Name: ${result.name}`, { showCancel: false });
         }
     }
 });
+*/
 
 SDK.init();
