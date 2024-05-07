1adf131faf5cd690870bd9b8518b4192a7a6e866
shaiju <rajan.shaiju@outlook.com>
2024-05-07 05:41:10 +0530

    refactored

--- src/extensions/test-plan-notify-menu/test-plan-notify-menu.ts
+++ src/extensions/test-plan-notify-menu/test-plan-notify-menu.ts
@@ -8,53 +8,17 @@ import { TestPoint, TestRestClient } from "azure-devops-extension-api/Test";
 SDK.register("notify-next-test-case-assignee", () => {
     return {
         execute: async (context: any[]) => {
+
             const selectedTestPoint:any=context[0];
             const projectName: string=selectedTestPoint["System.TeamProject"];
-
-            const witClient=await getClient(WorkItemTrackingRestClient);
+    
+            await getNextTestCaseDetails(selectedTestPoint);
             
-            /*const workItemPayload = {
-                "fields": {
-                    "System.Title": `Notify ${selectedTestPoint.assignedTo} on ${selectedTestPoint["System.Title"]}`,
-                    "System.AreaPath": `${projectName}\\Notifications`,
-                    "System.IterationPath": projectName,
-                }
-            };*/
-
-            const workItemPayload = [
-                {
-                    "op": "add",
-                    "path": "/fields/System.Title",
-                    "value": `Notification - Proceed with the test case - ${selectedTestPoint["System.Title"]} `
-                },
-                {
-                    "op": "add",
-                    "path": "/fields/System.Description",
-                    "value": `${selectedTestPoint.assignedTo}, please continue with the execution of ${selectedTestPoint["System.Title"]}. The previous test case just passed`
-                },
-                {
-                    "op": "add",
-                    "path": "/fields/System.AssignedTo",
-                    "value": selectedTestPoint.assignedTo
-                },
-                {
-                    "op": "add",
-                    "path": "/fields/System.AreaPath",
-                    "value": `${projectName}\\Notifications`
-                },
-                {
-                    "op": "add",
-                    "path": "/fields/System.IterationPath",
-                    "value": projectName
-                }
-            ];
-
+            const workItemPayload= await createNotification(selectedTestPoint);
 
             try {
-
+                const witClient=await getClient(WorkItemTrackingRestClient);
                 const createdNotification = await witClient.createWorkItem(workItemPayload, projectName, "Notification"); 
-                /*const task= await witClient.getWorkItem(9)  ;
-                console.log(task);*/
                 console.log(createdNotification);
                 const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
                 dialogSvc.openMessageDialog(`${selectedTestPoint.assignedTo} notified to start!!${selectedTestPoint["System.Title"]}`, { showCancel: false });
@@ -66,20 +30,68 @@ SDK.register("notify-next-test-case-assignee", () => {
     }
 });
 
-/*SDK.register("notify-next-test-case-assignee", () => {
-    return {
-        execute: async (context: any[]) => {
-            debugger;
-            console.log(context[0].System.Title);
-            const result = await getClient(BuildRestClient).getDefinition(context.project.id, context.id, undefined, undefined, undefined, true);
-            const result={name: "Shaiju"}
-            const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
-            dialogSvc.openMessageDialog(`Fetched build definition ${result.name}. Latest build: ${JSON.stringify(result.latestBuild)}`, { showCancel: false });
-            dialogSvc.openMessageDialog(`Name: ${result.name}, testSuiteId:${context.suite.name}`, { showCancel: false });
-            dialogSvc.openMessageDialog(`Name: ${result.name}`, { showCancel: false });
-        }
+async function getNextTestCaseDetails(selectedTestPoint:any)
+{
+
+    console.log('In: getNextTestCaseDetails');
+
+    const testClient=await getClient(TestRestClient);
+ 
+    if(testClient !=null){
+
+        console.log('Test Client Initialized');
+
+        const testCases=await testClient.getTestCases('project-101',3, selectedTestPoint.suiteId);
+
+        console.log('Test Cases Retrieved');
+    }else
+    {
+        console.log('Error initializing Test Client');
     }
-});
-*/
+
+    console.log('Out: getNextTestCaseDetails');
+    
+}
+
+async function createNotification(selectedTestPoint:any):Promise<any> {
+
+    console.log('In: createNotification');
+
+    const projectName: string=selectedTestPoint["System.TeamProject"];
+
+    const workItemPayload = [
+        {
+            "op": "add",
+            "path": "/fields/System.Title",
+            "value": `Notification - Proceed with the test case - ${selectedTestPoint["System.Title"]} `
+        },
+        {
+            "op": "add",
+            "path": "/fields/System.Description",
+            "value": `${selectedTestPoint.assignedTo}, please continue with the execution of ${selectedTestPoint["System.Title"]}. The previous test case just passed`
+        },
+        {
+            "op": "add",
+            "path": "/fields/System.AssignedTo",
+            "value": selectedTestPoint.assignedTo
+        },
+        {
+            "op": "add",
+            "path": "/fields/System.AreaPath",
+            "value": `${projectName}\\Notifications`
+        },
+        {
+            "op": "add",
+            "path": "/fields/System.IterationPath",
+            "value": projectName
+        }
+    ];
+
+    console.log('Out: createNotification');
+
+    return workItemPayload;
+
+}
+
 
 SDK.init();
